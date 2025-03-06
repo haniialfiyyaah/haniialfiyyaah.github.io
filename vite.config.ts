f21a0972dd4e6d139e2b2010682a2b81f1b471b1
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2025-03-06 13:57:30 +0700

    fix: Update routing and configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/haniialfiyyaah.github.io/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
