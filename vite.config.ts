f822b2a43e813447d8c2d456ae61fc44087c3588
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2025-03-06 15:11:48 +0700

    chore: Simplify Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
