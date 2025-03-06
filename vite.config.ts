df4dafc2e9c9cfee530db69e68e14e724d3f297c
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2025-03-06 14:22:59 +0700

    fix: Update asset paths to use relative URLs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/haniialfiyyaah.github.io/',
+  base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
 })
